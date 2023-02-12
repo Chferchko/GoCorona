ae869d5e9c4b3dc8e2979726667466776ef30ad3
Chferchko <3abolotskih@gmail.com>
2023-02-12 14:43:36 +0600

    added popup

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 - realized **menu-burger** by _JavaScript_
 - added **video** in two formats - _webm_ or _mp4_
 	- default media player **settings have been** slightly **changed** with _JavaScript_ and _media API | HTML5_
-
+	##### upd:
+- realized **popup** with **video** by _JavaScript_
 ---
-
 \* _click on the image to view the layout of the site in Figma_ 
 
 [![Figma layout](img/MD_image.png)](https://www.figma.com/file/mkFvHlRXrR6tES0CtmO0WB/Gocorna-Website?node-id=0%3A1&t=lmlcQIfyC8YOgfZz-1)
---
+++ js/video-popup.js
@@ -0,0 +1,82 @@
+const introVideoButton = $('.intro__play-button');
+const popup = $('.intro__popup');
+const popupVideo = $('.intro__popup-video');
+const popupVideoObject = document.getElementById("popup-video");
+const closeArea = popupVideoObject.closest('.intro__popup');
+let scrollBarWidth;
+
+function toggleButtonStatus() {
+	introVideoButton.toggleClass('active');
+};
+
+function toggleVisibilityPopup() {
+	popup.toggleClass('active');
+};
+
+function toggleVisibilityVideo() {
+	popupVideo.toggleClass('active');
+};
+
+function playPopupVideo() {
+	popupVideoObject.play();
+};
+
+function reloadPopupVideo() {
+	popupVideoObject.load();
+};
+
+function playPopupVideoAfterTime() {
+	setTimeout(playPopupVideo, 500);
+};
+
+function reloadPopupVideoAfterTime() {
+	setTimeout(reloadPopupVideo, 500);
+};
+
+function toggleLockDocumentBody() {
+	document.body.classList.toggle('active');
+};
+
+function toggleLockDocumentBodyAfterTime() {
+	setTimeout(toggleLockDocumentBody, 500);
+};
+
+let getScrollBarWidth = () => window.innerWidth - document.body.firstElementChild.clientWidth;
+
+scrollBarWidth = getScrollBarWidth();
+
+function lockDocumentContent() {
+	document.body.firstElementChild.style.paddingRight = `${scrollBarWidth}px`;
+	document.body.querySelector('header').style.paddingRight = `${scrollBarWidth}px`;
+};
+
+function unlockDocumentContent() {
+	document.body.firstElementChild.style.paddingRight = ``;
+	document.body.querySelector('header').style.paddingRight = ``;	
+}
+
+function unlockDocumentContentAfterTime() {
+	setTimeout(unlockDocumentContent, 500)
+}
+	
+function onClickToVideoButtonEventHandler(event) {
+	toggleButtonStatus();
+	toggleVisibilityPopup();
+	toggleVisibilityVideo();
+	toggleLockDocumentBody();
+	lockDocumentContent();
+	playPopupVideoAfterTime();
+};
+
+function onClickToCloseAreaEventHandler(e) {
+	if (!popup.is(e.target)) return;
+	toggleButtonStatus();
+	toggleVisibilityPopup();
+	toggleVisibilityVideo();
+	toggleLockDocumentBodyAfterTime()
+	unlockDocumentContentAfterTime();
+	reloadPopupVideoAfterTime();
+};
+
+introVideoButton.click(onClickToVideoButtonEventHandler);
+closeArea.onclick = onClickToCloseAreaEventHandler;
